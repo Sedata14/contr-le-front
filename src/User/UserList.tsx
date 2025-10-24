96758de254adf17c110392f5f245ef4c0eeebcbd
Sedata14 <132104929+Sedata14@users.noreply.github.com>
2025-10-24 14:14:50 +0200

    état de chargement (Loading…) et une erreur réseau

--- src/User/UserList.tsx
+++ src/User/UserList.tsx
@@ -1,24 +1,42 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import type { User } from '../model/User';
 import { UserCard } from './UserCard';
+import type { User } from '../model/User';
 
 export const UserList = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const res = await axios.get('https://dummyjson.com/users');
-      setUsers(res.data.users);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+
+      try {
+        const res = await axios.get('https://dummyjson.com/users');
+        setUsers(res.data.users);
+      } catch {
+        setError("j'arrive pas à charger chef");
+      } finally {
+        setLoading(false);
+      }
     };
     fetchUsers();
   }, []);
 
+  if (loading) return <p>loading...</p>;
+
+  if (error)
+    return (
+      <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+        <p>{error}</p>
+        <button onClick={() => window.location.reload()}>réessayer</button>
+      </div>
+    );
+
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem' }}>
-      {users.map(user => (
-        <UserCard key={user.id} user={user} />
-      ))}
+      {users.map(user => <UserCard key={user.id} user={user} />)}
     </div>
   );
 };
